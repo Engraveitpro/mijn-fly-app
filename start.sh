ede5a7a6c1db2d0ec026fe1e7eb419b1344f9dd8
Engrave-IT Pro <engraveitpro@gmail.com>
2025-05-19 14:35:35 +0200

    Replace start.sh to call wine64 and ensure correct format

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
+# Verwijder oude versie als die er is
+Remove-Item start.sh -ErrorAction Ignore
+
+# Creëer nieuwe start.sh zonder BOM, in ASCII, met LF-eindes
+@'
 #!/bin/sh
 exec wine64 /app/my_nuitka_app.exe --port "${PORT:-8080}"
+'@ | Set-Content -Path start.sh -Encoding ASCII
